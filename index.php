f2a8cac79b93c58254a6ab12e8578f53c08a3ae9
Matt DeClaire <matt@louddog.com>
2011-10-10 14:35:18 -0700

    delete users, add days option, show chopping block

--- index.php
+++ index.php
@@ -7,16 +7,11 @@ Version: 0.1
 Author URI: http://www.louddog.com/
 */
 
-if (!function_exists('debug')) {
-	function debug($var) {
-		echo "<pre>".print_r($var, true)."</pre>";
-	}
-}
-
 new User_Deleter();
 class User_Deleter {
 	var $defaults = array(
 		'enabled' => false,
+		'days' => 7,
 		'roles' => array(
 			'author',
 			'contributor',
@@ -25,12 +20,17 @@ class User_Deleter {
 	);
 	
 	function __construct() {
+		register_activation_hook(__FILE__, array(&$this, 'activate'));
 		register_deactivation_hook(__FILE__, array(&$this, 'deactivate'));
+
+		add_action('user_register', array(&$this, 'add_last_login'));
+		add_action('wp_login', array(&$this, 'save_last_login'));
+		
 		add_action('wp', array(&$this, 'schedule'));		
 		add_action('user_deleter', array(&$this, 'delete_inactive_users'));
 		add_action('admin_init', array(&$this, 'admin_init'));
 		add_action('admin_menu', array(&$this, 'admin_menu'));
-		add_action('admin_notices', array(&$this, 'admin_notices'));
+
 		$this->options = array_merge($this->defaults, get_option('user_deleter', array()));
 	}
 	
@@ -40,12 +40,49 @@ class User_Deleter {
 		}
 	}
 	
+	function activate() {
+		global $wpdb;
+		
+		$time = time();
+		$inserts = array();
+		foreach ($wpdb->get_col("select ID from $wpdb->users") as $ID) $inserts[] = "($ID, 'user_deleter_last_login', $time)";
+		if (count($inserts)) $wpdb->query("insert into $wpdb->usermeta (user_id, meta_key, meta_value) values ".implode(',', $inserts));
+		
+		wp_clear_scheduled_hook('user_deleter');
+	}
+	
 	function deactivate() {
+		global $wpdb;
+		$wpdb->query("delete from $wpdb->usermeta where meta_key = 'user_deleter_last_login'");
+		
 		wp_clear_scheduled_hook('user_deleter');
 	}
 	
 	function delete_inactive_users() {
-		// TODO: delete inactive users
+		foreach ($this->inactive_users() as $user_id) {
+			wp_delete_user($user_id);
+		}
+	}
+	
+	function inactive_users() {
+		$query = new WP_User_Query(array(
+			'meta_key' => 'user_deleter_last_login',
+			'meta_value' => time(),// - ($this->options['days'] * 86400), // 86400 = seconds in a day
+			'meta_compare' => '<=',
+			'fields' => array('ID'),
+		));
+		
+		$user_ids = array();
+		
+		foreach ($this->options['roles'] as $role) {
+			foreach ($query->results as $user) {
+				if (user_can($user->ID, $role)) {
+					$user_ids[] = $user->ID;
+				}
+			}
+		}
+		
+		return array_unique($user_ids);
 	}
 	
 	function admin_init() {
@@ -53,7 +90,7 @@ class User_Deleter {
 		add_settings_section('user_deleter', "User Deletion Settings", array(&$this, 'user_deletion_settings'), 'user_deleter');
 		add_settings_field('user_deleter_enabled', "Enabled", array(&$this, 'show_field_enabled'), 'user_deleter', 'user_deleter');
 		add_settings_field('user_deleter_roles', "Roles", array(&$this, 'show_field_roles'), 'user_deleter', 'user_deleter');
-		
+		add_settings_field('user_deleter_days', "Days", array(&$this, 'show_field_days'), 'user_deleter', 'user_deleter');
 	}
 	
 	function validate($input) {
@@ -67,6 +104,8 @@ class User_Deleter {
 				}
 			}
 		}
+		
+		if (is_numeric($input['days'])) $this->options['days'] = intval($input['days']);
 		
 		return $this->options;
 	}
@@ -105,6 +144,10 @@ class User_Deleter {
 		echo "</ul>";
 	}
 	
+	function show_field_days() { ?>
+		<input type="text" name="user_deleter[days]" value="<?php echo $this->options['days']; ?>" />
+	<?php }
+	
 	function admin_menu() {
 		add_options_page(
 			"User Deleter",
@@ -115,7 +158,7 @@ class User_Deleter {
 		);
 	}
 	
-	function settings() { ?>
+	function settings() { $endagered_user_ids = $this->inactive_users(); ?>
 		<form action="options.php" method="post">
 			<?php
 				settings_fields('user_deleter');
@@ -123,9 +166,25 @@ class User_Deleter {
 			?>
 			<input type="submit" value="Save Settings" />
 		</form>
+		
+		<h3>Tonight's Chopping Block (<?php echo count($endagered_user_ids); ?>)</h3>
+		<ul>
+			<?php foreach ($endagered_user_ids as $user_id) { $user = get_userdata($user_id); ?>
+				<li>
+					<a href="user-edit.php?user_id=<?php echo $user_id; ?>">
+						<?php echo $user->user_nicename; ?>
+					</a>
+				</li>
+			<?php } ?>
+		</ul>
 	<?php }
 	
-	function admin_notices() {
-		
+	function add_last_login($id) {
+		update_user_meta($id, 'user_deleter_last_login', time());
+	}
+	
+	function save_last_login($login) {
+		$user = get_user_by('login', $login);
+		update_user_meta($user->ID, 'user_deleter_last_login', time());
 	}
 }
